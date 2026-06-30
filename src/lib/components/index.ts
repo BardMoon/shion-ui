168685c384fcb37e5cb814fe68c871763bf998d9
BardMoon <came.from.the.north7239@gmail.com>
2026-07-01 03:27:51 +0900

    fix index

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,3 +3,6 @@ export { default as DropdownButton } from "./button/DropdownButton.svelte";
 
 export { default as Dropdown } from "./dropdown/Dropdown.svelte";
 export { default as DropdownItem } from "./dropdown/DropdownItem.svelte";
+
+export { default as Separator } from "./misc/Separator.svelte";
+export { default as Popover } from "./popover/Popover.svelte";
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,2 @@
-export { default as Button } from "./components/button/Button.svelte";
-export { default as DropdownButton } from "./components/button/DropdownButton.svelte";
-
-export { default as Dropdown } from "./components/dropdown/Dropdown.svelte";
-export { default as DropdownItem } from "./components/dropdown/DropdownItem.svelte";
+export * from "./components";
+export * from "./types";
