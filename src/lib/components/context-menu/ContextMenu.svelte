15909121b7725896706528e69b427cd541674611
BardMoon <came.from.the.north7239@gmail.com>
2026-07-03 16:39:09 +0900

    add Context Menu

---
+++ src/lib/components/context-menu/ContextMenu.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { getContextMenu } from "./context-menu.svelte.js";
+  import { Dropdown } from "@shiola/ui";
+
+  const contextMenu = getContextMenu();
+</script>
+
+<Dropdown
+  class="fixed z-50 p-1"
+  style={`left:${contextMenu.x}px;top:${contextMenu.y}px;`}
+  items={contextMenu.items}
+  open={contextMenu.open}
+  onclose={contextMenu.hide}
+  onclick={contextMenu.hide}
+/>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -6,3 +6,6 @@ export { default as Dropdown } from "./dropdown/Dropdown.svelte";
 export { default as Popover } from "./popover/Popover.svelte";
 export { default as Separator } from "./misc/Separator.svelte";
 export { rovingFocusItem } from "./misc/rovingFocus";
+
+export { default as ContextMenu } from "./context-menu/ContextMenu.svelte";
+export { contextMenu } from "./context-menu/context-menu.svelte";
