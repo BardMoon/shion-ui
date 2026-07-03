fa1509d512afde44290f790a0eea69bc71450cd3
BardMoon <came.from.the.north7239@gmail.com>
2026-07-03 17:32:02 +0900

    use snippet in ContextMenu

--- src/lib/components/context-menu/ContextMenu.svelte
+++ src/lib/components/context-menu/ContextMenu.svelte
@@ -1,33 +1,23 @@
 <script lang="ts">
-  import { getContextMenu } from "./context-menu.svelte.js";
-  import { Dropdown } from "$lib/components";
+  import type { Snippet } from "svelte";
   import type { MenuItemType } from "$lib/types";
+  import { Dropdown } from "$lib/components";
+  import { getContextMenu } from "./context-menu.svelte.js";
 
   const contextMenu = getContextMenu();
+  type Props = {
+    content: Snippet<[MenuItemType]>;
+  };
+
+  let { content }: Props = $props();
 </script>
 
 <Dropdown
+  {content}
   class="fixed z-50 p-1"
   style={`left:${contextMenu.x}px;top:${contextMenu.y}px;`}
   items={contextMenu.items}
   open={contextMenu.open}
   onclose={contextMenu.hide}
   onclick={contextMenu.hide}
->
-  {#snippet content(item: MenuItemType)}
-    {#if item.type === "item"}
-      {@const Icon = item.icon}
-      {#if Icon}
-        <Icon size={13} />
-      {/if}
-      <span
-        class={[
-          "flex-1 min-w-0 overflow-hidden text-ellipsis whitespace-nowrap",
-        ]}>{item.label}</span
-      >
-      {#if item.shortcut}
-        <span class={["shortcut", "shrink-0 mr-0.5"]}>{item.shortcut}</span>
-      {/if}
-    {/if}
-  {/snippet}
-</Dropdown>
+/>
